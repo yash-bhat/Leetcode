4fb2e85f35619b61a15b5473581e12399d1ab821
Yashaswini Bhat <hvyashbhat@gmail.com>
2020-03-10 20:45:48 -0400

    Create Valid Anagram.py

---
+++ Valid Anagram.py
@@ -0,0 +1,30 @@
+"""
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+"""
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        import collections
+        
+        first = collections.Counter(s)
+        second = collections.Counter(t)
+        
+        if first == second:
+            return 1
+        else:
+            return 0
