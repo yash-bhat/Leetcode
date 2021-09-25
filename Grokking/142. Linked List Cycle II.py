77e24f679ba005637e81927db0136e49045ac977
Yashaswini Bhat <hvyashbhat@gmail.com>
2021-09-24 19:52:29 -0700

    Update 142. Linked List Cycle II.py

--- Grokking/142. Linked List Cycle II.py
+++ Grokking/142. Linked List Cycle II.py
@@ -32,3 +32,35 @@
 # -105 <= Node.val <= 105
 # pos is -1 or a valid index in the linked-list.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if head is None:
+            return None
+        
+        def meet_point(head):
+            slow = head
+            fast = head
+            
+            while fast is not None and fast.next is not None:
+                slow = slow.next
+                fast = fast.next.next
+                if slow == fast:
+                    return slow
+            return None
+        
+        slow = head
+        fast = meet_point(head)
+        if fast == None:
+            return None
+        
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
+        
